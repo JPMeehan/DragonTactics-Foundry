d249a5d517972af5e15ca8838f4feac4a28e5ae9
Joseph Meehan <Meehan_Joseph@bah.com>
2020-05-13 12:55:49 -0400

    0.4.1
    Many fixes, handle it!

--- dragontactics/scripts/actors/base.js
+++ dragontactics/scripts/actors/base.js
@@ -28,9 +28,8 @@ export class DragonTacticsActorSheet extends ActorSheet {
     if (!this.options.editable) return;
 
     html.find('.defense-edit').click(ev => {
-      try {html.find('.defense-config.show').removeClass('show')}
-      catch (e) {html.find('.defense-config').addClass('show')}
-    })
+      html.find('.defense-config').toggleClass('show')
+    });
 
     // Update Inventory Item
     html.find('.item-edit').click(ev => {
@@ -45,6 +44,80 @@ export class DragonTacticsActorSheet extends ActorSheet {
       this.actor.deleteOwnedItem(li.data("itemId"));
       li.slideUp(200, () => this.render(false));
     });
+
+  }
+
+  
+
+  getData() {
+    const data = super.getData();
+    
+    // Prepare items.
+    if (this.actor.data.type == 'hero') {
+      this._prepareHeroItems(data);
+    }
+
+    return data;
+  }
+
+  /**
+   * Organize and classify Items for Character sheets.
+   *
+   * @param {Object} actorData The actor to prepare.
+   *
+   * @return {undefined}
+   */
+  _prepareHeroItems(sheetData) {
+    const actorData = sheetData.actor;
+
+    // Initialize containers.
+    const cls = [];
+    // const equipment = { // armor, implement, supplies, weapon
+    //   "armor": [],
+    //   "worn": [],
+    //   "carried": [],
+    //   "home": []
+    // };
+    const features = { // feature
+      "race": [],
+      "class": [],
+      "feat": [],
+      "flaw": [],
+      "competency": []
+    };
+    const powers = {}; // power
+    const rituals = []; // ritual (need to add to sheet)
+
+    // Iterate through items, allocating to containers
+    for (let i of sheetData.items) {
+      let item = i.data;
+      i.img = i.img || DEFAULT_TOKEN;
+
+      if (i.type === 'class') {
+        cls.push(i);
+        cls.name = item.name;
+        cls.defense = item.data.defense;
+      }
+      else if (i.type === 'race') {
+        actorData.race = item.name;
+      }
+      else if (i.type === 'feature') features[item.data.type].push(i);
+      else if (i.type === 'powers') powers.push(i);
+      else if (i.type === 'ritual') rituals.push(i);
+    }
+
+    while (cls.length > 1) {
+      console.log("Deleting " + cls[0].data.name);
+      delete cls[0];
+      cls.shift();
+    }
+
+    // Assign and return
+    actorData.class.name = cls[0].data.name;
+    actorData.class.defense = cls[0].data.defense;
+    actorData.powers = powers;
+    actorData.features = features;
+    actorData.equipment.rituals = rituals;
 
   }
 }
--- dragontactics/scripts/actors/entity.js
+++ dragontactics/scripts/actors/entity.js
@@ -38,8 +38,8 @@ export class DragonTacticsActor extends Actor {
     data.reflex.value = 10 + Math.max(data.abilities.dexterity.mod, data.abilities.intelligence.mod) + data.class.quest + data.reflex.miscbonus + data.equipment.worn.arms.shield;
     data.will.value = 10 + Math.max(data.abilities.wisdom.mod, data.abilities.charisma.mod) + data.class.quest + data.will.miscbonus;
     
-    data.health.max = data.class.defense.hpBase + data.abilities.constitution.score + (data.class.level - 1) * data.class.hpLevel + data.bonus.hp;
-    data.surges.max = data.class.defense.surges + data.abilities.constitution.mod + data.bonus.surges;
+    data.health.max = data.class.defense.hpBase + data.abilities.constitution.score + (data.class.level - 1) * data.class.defense.hpLevel + data.class.bonus.hp;
+    data.surges.max = data.class.defense.surges + data.abilities.constitution.mod + data.class.bonus.surges;
 
     for (let skill of Object.values(data.skill)) {
       skill.rank_bonus = this.training(skill.rank);
@@ -72,82 +72,6 @@ export class DragonTacticsActor extends Actor {
   armorabil(obj, prop) {
     return( obj == null ? 0 : obj[prop] );
   }
-  /**
-   * Organize and classify Items for Character sheets.
-   *
-   * @param {Object} actorData The actor to prepare.
-   *
-   * @return {undefined}
-   */
-
-  getData() {
-    const data = super.getData();
-    data.dtypes = ["String", "Number", "Boolean"];
-    for (let attr of Object.values(data.data.attributes)) {
-      attr.isCheckbox = attr.dtype === "Boolean";
-    }
-
-    // Prepare items.
-    if (this.actor.data.type == 'character') {
-      this._prepareCharacterItems(data);
-    }
-
-    return data;
-  }
-
-
-  _prepareCharacterItems(sheetData) {
-    const actorData = sheetData.actor;
-
-    // Initialize containers.
-    const cls = [];
-    const equipment = { // armor, implement, supplies, weapon
-      "armor": [],
-      "worn": [],
-      "carried": [],
-      "home": []
-    };
-    const features = { // feature
-      "race": [],
-      "class": [],
-      "feat": [],
-      "flaw": [],
-      "competency": []
-    };
-    const powers = {}; // power
-    const rituals = []; // ritual (need to add to sheet)
-
-    // Iterate through items, allocating to containers
-    for (let i of sheetData.items) {
-      let item = i.data;
-      i.img = i.img || DEFAULT_TOKEN;
-
-      if (i.type === 'class') {
-        cls.push(i);
-        cls.name = item.name;
-        cls.defense = item.data.defense;
-      }
-      else if (i.type === 'race') {
-        actorData.race = item.name;
-      }
-      else if (i.type === 'feature') features[item.data.type].push(i);
-      else if (i.type === 'powers') powers.push(i);
-      else if (i.type === 'ritual') rituals.push(i);
-    }
-
-    while (cls.length > 1) {
-      console.log("Deleting " + cls[0].data.name);
-      delete cls[0];
-      cls.shift();
-    }
-
-    // Assign and return
-    actorData.class.name = cls[0].data.name;
-    actorData.class.defense = cls[0].data.defense;
-    actorData.powers = powers;
-    actorData.features = features;
-    actorData.equipment.rituals = rituals;
-
-  }
+  
   /* */
 }
