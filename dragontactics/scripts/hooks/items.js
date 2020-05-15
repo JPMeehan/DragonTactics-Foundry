b1725619a3e11787f84780cd217a5b5c67ff38f8
Joseph Meehan <Meehan_Joseph@bah.com>
2020-05-15 09:23:27 -0400

    0.4.20
    All the hook fixes

--- dragontactics/scripts/hooks/items.js
+++ dragontactics/scripts/hooks/items.js
@@ -1,14 +1,17 @@
+
+
+
 Hooks.on('createOwnedItem', (actor, item) => {
-  console.log("Creating an Item")
+  const data = actor.data.data;
   switch (item.type) {
     case "class":
-      var cls = actor.data.data.class;
-      if (cls._id != "") {actor.deleteOwnedItem(cls._id)}
-      cls.name = item.name;
-      cls._id = item._id;
+      if (data.class._id != "") {actor.deleteOwnedItem(data.class._id)}
+      data.class.name = item.name;
+      data.class._id = item._id;
       for (let [key, value] of Object.entries(item.data.defense)) {
-        cls.defense[key] = value;
+        data.class.defense[key] = value;
       }
+      
       var features = [];
       
       for(let [key, value] of Object.entries(item.data.features)) {
@@ -17,13 +20,13 @@ Hooks.on('createOwnedItem', (actor, item) => {
           "description": value.value
         });
       }
-      actor.data.data.features.class = [...features];
+      actor.update({"actor.data.data.features.class" : features});
       break;
     case "race":
-      var race = actor.data.data.race;
-      if (race._id != "") {actor.deleteOwnedItem(race._id)}
-      race.name = item.name;
-      race._id = item._id;
+      if (data.race._id != "") {actor.deleteOwnedItem(data.race._id)}
+      data.race.name = item.name;
+      data.race._id = item._id;
+      
       var features = [];
       
       for(let [key, value] of Object.entries(item.data.features)) {
@@ -32,22 +35,46 @@ Hooks.on('createOwnedItem', (actor, item) => {
           "description": value.value
         });
       }
-      actor.data.data.features.race = [...features];
+      actor.update({"actor.data.data.features.race" : features});
       break;
+    case "feature":
+      var features = data.features[item.data.type];
+      features.push({
+        "_id": item._id,
+        "label": item.name,
+        "description": item.data.description
+      })
+      actor.update({[`actor.data.data.features.${item.data.type}`] : features});
+      break;
+    // /*
+    case "power":
+      const newpower = {
+        "_id": item._id
+      }
+      for(let [key, value] of Object.entries(item.data)) {newpower[key] = value;}
+      data.powers.push(newpower);
+      actor.update({"actor.data.data.powers" : data.powers});
+
+    /* */
   }
+  actor.update({"data" : data})
 });
 
-Hooks.on('updateOwnedItem', (actor, item) => {
+Hooks.on('updateOwnedItem', (actor, item, delta) => {
+  function matchID(object) {
+    return object["_id"] === item._id;
+  }
+  const data = actor.data.data;
   // console.log("Updating an item")
   // console.log(actor);
   // console.log(item)
   switch (item.type) {
     case "class":
-      var cls = actor.data.data.class;
-      cls.name = item.name;
+      data.class.name = item.name;
       for (let [key, value] of Object.entries(item.data.defense)) {
-        cls.defense[key] = value;
+        data.class.defense[key] = value;
       }
+      
       var features = [];
       
       for(let [key, value] of Object.entries(item.data.features)) {
@@ -56,11 +83,11 @@ Hooks.on('updateOwnedItem', (actor, item) => {
           "description": value.value
         });
       }
-      actor.data.data.features.class = [...features];
+      actor.update({"actor.data.data.features.class" : features});
       break;
     case "race":
-      var race = actor.data.data.race;
-      race.name = item.name;
+      data.race.name = item.name;
+      
       var features = [];
       
       for(let [key, value] of Object.entries(item.data.features)) {
@@ -69,7 +96,26 @@ Hooks.on('updateOwnedItem', (actor, item) => {
           "description": value.value
         });
       }
-      actor.data.data.features.race = [...features];
+      actor.update({"actor.data.data.features.race" : features});
       break;
+    case "feature":
+      var features = data.features[item.data.type];
+      features[features.findIndex(matchID)][Object.keys(delta)[0]] = Object.values(delta)[0]
+      // for(let i of features){
+      //   if(i._id === delta._id){
+      //     i[Object.keys(delta)[0]] = delta[Object.keys(delta)[0]]
+      //     // try {i.name=delta.name;} catch (e) {console.log("Delta did not have a name")}
+      //     // try {i.description=delta.description} catch (e) {console.log("Delta did not have a description")}
+      //   }
+      // }
+      actor.update({[`actor.data.data.features.${item.data.type}`] : features});
+      break;
+    // /*
+    case "power":
+      var powers = data.powers;
+      powers[powers.findIndex(matchID)][Object.keys(delta)[0]] = Object.values(delta)[0]
+      
+    /* */
   }
+  actor.update({"data" : data})
 });
