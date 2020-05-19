7cd3521f0aa1015394a6393754d50a1f7e239527
Joseph Meehan <Meehan_Joseph@bah.com>
2020-05-19 14:29:48 -0400

    0.5.13
    Final fixes to powers hopefully

--- dragontactics/scripts/hooks/items.js
+++ dragontactics/scripts/hooks/items.js
@@ -1,41 +1,42 @@
-
-
-
 Hooks.on('createOwnedItem', (actor, item) => {
-  const data = actor.data.data;
+  const data = duplicate(actor.data.data);
   switch (item.type) {
     case "class":
-      if (data.class._id != "") {actor.deleteOwnedItem(data.class._id)}
+      if (data.class._id != "") {
+        actor.deleteOwnedItem(data.class._id)
+      }
       data.class.name = item.name;
       data.class._id = item._id;
       for (let [key, value] of Object.entries(item.data.defense)) {
         data.class.defense[key] = value;
       }
-      
+
       var features = [];
-      
-      for(let [key, value] of Object.entries(item.data.features)) {
+
+      for (let [key, value] of Object.entries(item.data.features)) {
         features.push({
           "label": value.label,
           "description": value.value
         });
       }
-      actor.update({"data.features.class" : features});
+      // actor.update({"data.features.class" : features});
       break;
     case "race":
-      if (data.race._id != "") {actor.deleteOwnedItem(data.race._id)}
+      if (data.race._id != "") {
+        actor.deleteOwnedItem(data.race._id)
+      }
       data.race.name = item.name;
       data.race._id = item._id;
-      
+
       var features = [];
-      
-      for(let [key, value] of Object.entries(item.data.features)) {
+
+      for (let [key, value] of Object.entries(item.data.features)) {
         features.push({
           "label": value.label,
           "description": value.value
         });
       }
-      actor.update({"data.features.race" : features});
+      // actor.update({"data.features.race" : features});
       break;
     case "feature":
       var features = data.features[item.data.type] || {};
@@ -44,14 +45,16 @@ Hooks.on('createOwnedItem', (actor, item) => {
         "description": item.data.description
       };
       features[item._id] = newfeature;
-      actor.update({[`data.features.${item.data.type}`] : features});
+      // actor.update({[`data.features.${item.data.type}`] : features});
       break;
     case "power":
       const newpower = {
         "name": item.name
       }
-      for(let [key, value] of Object.entries(item.data)) {newpower[key] = value;}
-      switch(newpower.recharge) {
+      for (let [key, value] of Object.entries(item.data)) {
+        newpower[key] = value;
+      }
+      switch (newpower.recharge) {
         case "":
           newpower["usage"] = "atwill";
           break;
@@ -63,60 +66,61 @@ Hooks.on('createOwnedItem', (actor, item) => {
           newpower["usage"] = "daily";
       }
       data.powers[item._id] = newpower;
-      actor.update({"data.powers" : data.powers});
+      // actor.update({"data.powers" : data.powers});
   }
-  actor.update({"data" : data})
+  actor.update({
+    "data": data
+  })
 });
 
 Hooks.on('updateOwnedItem', (actor, item, delta) => {
-  const data = actor.data.data;
+  const data = duplicate(actor.data.data);
   switch (item.type) {
     case "class":
       data.class.name = item.name;
       for (let [key, value] of Object.entries(item.data.defense)) {
         data.class.defense[key] = value;
       }
-      
+
       var features = [];
-      
-      for(let [key, value] of Object.entries(item.data.features)) {
+
+      for (let [key, value] of Object.entries(item.data.features)) {
         features.push({
           "label": value.label,
           "description": value.value
         });
       }
-      actor.update({"data.features.class" : features});
+      // actor.update({"data.features.class" : features});
       break;
     case "race":
       data.race.name = item.name;
-      
+
       var features = [];
-      
-      for(let [key, value] of Object.entries(item.data.features)) {
+
+      for (let [key, value] of Object.entries(item.data.features)) {
         features.push({
           "label": value.label,
           "description": value.value
         });
       }
-      actor.update({"data.features.race" : features});
+      // actor.update({"data.features.race" : features});
       break;
     case "feature":
       var target = Object.keys(delta.data)[0];
       var features = data.features[item.data.type] || {};
-      if(target === "type") {
+      if (target === "type") {
         var oldtype;
         var oldfeatures;
         const featuretypes = ["feat", "competency", "flaw"]
 
         // go through eleigible lists
-        for(let type = 0; type<3; type++){
-          if(featuretypes[type] === item.data.type) {
+        for (let type = 0; type < 3; type++) {
+          if (featuretypes[type] === item.data.type) {
             features[item._id] = {
               "label": item.name,
               "description": item.data.description
             }
-          }
-          else {
+          } else {
             if (data.features[featuretypes[type]][item._id]) {
               oldtype = featuretypes[type];
               oldfeatures = data.features[oldtype] || {};
@@ -125,25 +129,26 @@ Hooks.on('updateOwnedItem', (actor, item, delta) => {
           }
         }
 
-        actor.update({
-          [`data.features.${item.data.type}`] : features,
-          [`data.features.${oldtype}`]: oldfeatures
-      });
-      }
-      else {
+        // actor.update({
+        //   [`data.features.${item.data.type}`] : features,
+        //   [`data.features.${oldtype}`]: oldfeatures
+        // });
+      } else {
         features[item._id] = {
           "label": item.name,
           "description": item.data.description
         }
-        actor.update({[`data.features.${item.data.type}`] : features});
+        // actor.update({[`data.features.${item.data.type}`] : features});
       }
       break;
     case "power":
       const newpower = {
         "name": item.name
       }
-      for(let [key, value] of Object.entries(item.data)) {newpower[key] = value;}
-      switch(newpower.recharge) {
+      for (let [key, value] of Object.entries(item.data)) {
+        newpower[key] = value;
+      }
+      switch (newpower.recharge) {
         case "":
           newpower["usage"] = "atwill";
           break;
@@ -155,11 +160,9 @@ Hooks.on('updateOwnedItem', (actor, item, delta) => {
           newpower["usage"] = "daily";
       }
       data.powers[item._id] = newpower;
-      actor.update({"data.powers" : data.powers});
+      // actor.update({"data.powers" : data.powers});
   }
-  actor.update({"data" : data})
+  actor.update({
+    "data": data
+  })
 });
-
-function _calculatePower(power) {
-  return power;
-};
