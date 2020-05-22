5a011f4bc63e86dd4c29a8b77443715d29c7269d
Joseph Meehan <Meehan_Joseph@bah.com>
2020-05-22 16:01:47 -0400

    Update radioFix.js

--- radioFix.js
+++ radioFix.js
@@ -1,52 +1,55 @@
 // _getFormData
 
-if ( form[el.name] instanceof RadioNodeList ) {
+if (form[el.name] instanceof RadioNodeList) {
     const inputs = Array.from(form[el.name]);
-    if ( inputs.every(i => i.disabled) ) FD.delete(k);
+    if (inputs.every(i => i.disabled)) FD.delete(k);
     let values = inputs.map(i => i.type === "checkbox" ? i.checked || false : i.value);
     FD.set(el.name, JSON.stringify(values));
     dtypes[el.name] = "Radio";
 }
 
-if ( form[el.name] instanceof RadioNodeList ) {
+if (form[el.name] instanceof RadioNodeList) {
     const inputs = Array.from(form[el.name]);
-    if ( inputs.every(i => i.disabled) ) FD.delete(k);
+    if (inputs.every(i => i.disabled)) FD.delete(k);
     let values = inputs.map(i => i.type === "checkbox" ? i.checked || false : i.value);
     FD.set(el.name, JSON.stringify(values));
     dtypes[el.name] = "Radio";
 }
 
-if ( form[el.name] instanceof RadioNodeList ) {
-        
+if (form[el.name] instanceof RadioNodeList) {
+
     const inputs = Array.from(form[el.name]);
-    if ( inputs.every(i => i.disabled) ) FD.delete(k);
-    
+    if (inputs.every(i => i.disabled)) FD.delete(k);
+
     let values = "";
     values = inputs.map(i => i.checked ? i.value : false).filter(i => i);
-    
+
     FD.set(el.name, JSON.stringify(values[0]));
     dtypes[el.name] = 'Radio';
-  }
+}
 
 
 // _render
 
-if ( focus && focus.name ) {
+if (focus && focus.name) {
     const input = this.form[focus.name];
-    if ( input ) input.focus();
-  }
+    if (input) input.focus();
+}
 
-if ( focus && focus.name ) {
+if (focus && focus.name) {
     const input = this.form[focus.name];
-    if ( input ) { 
-        if (input  instanceof RadioNodeList) {
+    if (input) {
+        if (input instanceof RadioNodeList) {
             var target;
-          function matchValue(currentValue, index, arr) {
-              if (currentValue.value === focus.value) {target = focus}
-          }
+            function matchValue(currentValue, index, arr) {
+                if (currentValue.value === focus.value) {
+                    target = focus
+                }
+            }
             input.forEach(matchValue)
             target.focus()
+        } else {
+            input.focus()
         }
-        else {input.focus()}
     };
 }
