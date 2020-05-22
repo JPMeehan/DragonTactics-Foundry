c794aab0f1aad41218ae12c66bf46b678cc50d89
Joseph Meehan <Meehan_Joseph@bah.com>
2020-05-22 16:43:10 -0400

    0.6.12
    Handlebars debugging is the worst

--- dragontactics/scripts/actors/entity.js
+++ dragontactics/scripts/actors/entity.js
@@ -32,8 +32,9 @@ export class DragonTacticsActor extends Actor {
 
   _prepareHeroData(actorData) {
     const data = actorData.data;
+
     
-    data.ac.value = 10 + this.armorabil(data.abilities[data.equipment.worn.armor.ability],"score")  + data.class.quest + data.ac.miscbonus + data.equipment.armor.torso.bonus + data.equipment.armor.shield.bonus;
+    data.ac.value = 10 + this.armorabil(data.abilities[data.equipment.armor.ability],"score")  + data.class.quest + data.ac.miscbonus + data.equipment.armor.torso.bonus + data.equipment.armor.shield.bonus;
     data.fortitude.value = 10 + Math.max(data.abilities.strength.mod, data.abilities.constitution.mod) + data.class.quest + data.fortitude.miscbonus;
     data.reflex.value = 10 + Math.max(data.abilities.dexterity.mod, data.abilities.intelligence.mod) + data.class.quest + data.reflex.miscbonus + data.equipment.armor.shield.bonus;
     data.will.value = 10 + Math.max(data.abilities.wisdom.mod, data.abilities.charisma.mod) + data.class.quest + data.will.miscbonus;
