200eccba24afef9dab32078711908a5051ec0a76
Joseph Meehan <Meehan_Joseph@bah.com>
2020-05-26 14:33:52 -0400

    0.8.0
    Starting to implement initiative

---
+++ dragontactics/scripts/initiative/setupHandler.ts
@@ -0,0 +1,13 @@
+import { createActionCardTable } from './util'
+
+export class SwadeSetup {
+
+    static async setup(): Promise<void> {
+
+        if (!game.tables.getName(CONFIG.SWADE.init.cardTable)) {
+            await createActionCardTable();
+            ui.notifications.info('First-Time-Setup complete');
+        }
+    }
+
+}
