a6e0f46e0405248423914c19c14ae32411ac43ce
Joseph Meehan <Meehan_Joseph@bah.com>
2020-05-28 14:53:22 -0400

    0.9.0
    Attempted mass compendium import

--- dragontactics/scripts/setupHandler.js
+++ dragontactics/scripts/setupHandler.js
@@ -5,5 +5,53 @@ export class DragonTacticsSetup {
             await createActionCardTable();
             ui.notifications.info('First-Time-Setup complete');
         }
+        characterCreation = ["equipment", "classes", "powers", "races", "rituals", "feats", "competencies", "flaws"]
+
+        characterCreation.forEach((value, index, array) => {
+            var pack = game.packs.find(p => p.collection === `dragontactics.${value}`);
+            var response;
+            if (value === "equipment") {
+                response = await fetch("systems/dragontactics/assets/packjson/mundaneEquipment.json")
+            }
+            if (value === "classes") {
+                response = await fetch("systems/dragontactics/assets/packjson/classes.json")
+            }
+            if (value === "powers") {
+                response = await fetch("systems/dragontactics/assets/packjson/basicpowers.json")
+            }
+            if (value === "races") {
+                response = await fetch("systems/dragontactics/assets/packjson/races.json")
+            }
+            if (value === "rituals") {
+                response = await fetch("systems/dragontactics/assets/packjson/foundryrituals.json") //
+            }
+            if (value === "feats") {
+                response = await fetch("systems/dragontactics/assets/packjson/feats-level1.json")
+            }
+            if (value === "competencies") {
+                response = await fetch("systems/dragontactics/assets/packjson/competencies.json") //
+            }
+            if (value === "flaws") {
+                response = await fetch("systems/dragontactics/assets/packjson/flaws.json");
+            }
+            const content = await response.json();
+            let items = []
+            for (let i of content["data"]) {
+                let item = {
+                    "name": i.name,
+                    "type": value,
+                    "data": i
+                }
+                delete item.data.name;
+                items.push(item)
+            }
+
+            const temp = await Item.create(items, {temporary: true})
+
+            for ( let j of temp ) {
+                await pack.importEntity(j);
+                console.log(`Imported Item ${j.name} into Compendium pack ${pack.collection}`);
+            }
+        });
     }
 }
